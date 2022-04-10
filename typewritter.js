26bf2811ada1e62f88697bd8502d5861b5cdfa72
Yoyo Yang <yoyo.truly@gmail.com>
2022-04-09 21:26:02 -0400

    Format code to be on one line

--- typewritter.js
+++ typewritter.js
@@ -2,12 +2,8 @@ const sentence = "hello there from lighthouse labs";
 
 let delay = 0;
 for (const char of sentence) {
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, delay)
+  setTimeout(() => process.stdout.write(char), delay)
   delay += 50;
 }
 
-setTimeout(() => {
-  console.log();
-}, delay + 50);
+setTimeout(() => console.log(), delay + 50);
